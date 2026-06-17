4b36ab6cca7dd67e5990996bd086a7d5be748b21
Huỳnh Minh Quân <minhquanthcs257@gmail.com>
2026-06-17 08:57:31 +0700

    clean

--- app/src/main/java/com/example/giaodien/data/model/BinhLuanRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.giaodien.data.network.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class BinhLuanRequest(
-    val thucDonId: Long,
-    val noiDung: String
-)
--- app/src/main/java/com/example/giaodien/data/model/DanhGiaRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.giaodien.data.network.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class DanhGiaRequest(
-    val thucDonId: Long,
-    val soSao: Int
-)
